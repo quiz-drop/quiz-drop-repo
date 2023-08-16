f65c2b25cf94e6e5a7800e3a45db04b05e0e1f40
yuio7279 <yuio7279@gmail.com>
2023-08-16 15:59:31 +0900

    init commit

---
+++ src/main/java/com/sparta/quizdemo/QuizdemoApplication.java
@@ -0,0 +1,14 @@
+package com.sparta.quizdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class QuizdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/quizdemo/QuizdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.quizdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
