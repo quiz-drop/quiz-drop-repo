ea2cb67b3a6f33f9d056803a19af2d32b55d5a7d
yuio7279 <yuio7279@gmail.com>
2023-08-16 22:23:31 +0900

    [FEAT] : 스프링 시큐리티/JWT토큰 회원가입 및 로그인

---
+++ src/main/java/com/sparta/quizdemo/common/entity/User.java
@@ -0,0 +1,68 @@
+package com.sparta.quizdemo.common.entity;
+
+import com.sparta.quizdemo.user.SignupRequestDto;
+import com.sparta.quizdemo.user.UserRoleEnum;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Long  point;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+
+//    //user는 여러개의 post를 가질 수 있음
+//    @OneToMany(mappedBy = "user")
+//    List<Post> postList = new ArrayList<>();
+//
+//    //user는 여러개의 comment를 가질 수 있음
+//    @OneToMany(mappedBy = "user")
+//    List<Comment> commentList = new ArrayList<>();
+
+
+//    public void addPostList(Post post){
+//        this.postList.add(post);
+//        post.setUser(this);
+//    }
+
+
+
+    //회원가입 생성자
+    public User(SignupRequestDto requestDto, UserRoleEnum role) {
+        this.username = requestDto.getUsername();
+        this.password = requestDto.getPassword();
+        this.email = requestDto.getEmail();
+        this.nickname = requestDto.getNickname();
+        this.point = 0L;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/sparta/quizdemo/user/UserController.java
@@ -0,0 +1,40 @@
+package com.sparta.quizdemo.user;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user/signup")
+    public void signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+        userService.signup(requestDto);
+//        return ResponseEntity.ok().body(new ApiResponseDto("회원가입 완료", HttpStatus.CREATED.value()));
+    }
+}
