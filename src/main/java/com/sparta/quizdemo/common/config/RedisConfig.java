a2c638f13ead834dca186085242513a4217a75e1
jiinse0 <jee_ji@naver.com>
2023-08-23 16:05:36 +0900

    [REFACTOR] 중복된 메서드 삭제

--- src/main/java/com/sparta/quizdemo/common/config/RedisConfig.java
+++ src/main/java/com/sparta/quizdemo/common/config/RedisConfig.java
@@ -1,16 +1,17 @@
 package com.sparta.quizdemo.common.config;
 
+import com.sparta.quizdemo.chat.service.RedisSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.Topic;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -25,15 +26,13 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int redisPort;
 
-    // Redis 저장소와 연결
+    /* Redis 저장소와 연결 */
     @Bean
     public RedisConnectionFactory connectionFactory() {
         return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
-    /**
-     * redis pub/sub 메시지를 처리하는 listener 설정
-     */
+    /* redis pub/sub 메시지를 처리하는 listener 설정 */
     @Bean
     public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
@@ -41,22 +40,20 @@ public class RedisConfig {
         return container;
     }
 
+    /*
+    * RedisMessageListenerContainer 로부터 메시지를 dispatch 받고,
+    * 실제 메시지를 처리하는 비즈니스 로직이 담긴 Subscriber Bean 추가
+    */
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
-        redisConfig.setHostName(redisHost);
-        redisConfig.setPort(redisPort);
-        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisConfig);
-        return connectionFactory;
+    public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) {
+        return new MessageListenerAdapter(subscriber, "onMessage");
     }
 
-    /**
-     * 어플리케이션에서 사용할 redisTemplate 설정
-     */
+    /* 어플리케이션에서 사용할 redisTemplate 설정 */
     @Bean
-    public RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory());
 
         // 일반적인 key:value의 경우 시리얼라이저
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -69,7 +66,8 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    private Topic createTopic() {
+    @Bean
+    public Topic createTopic() {
         return new ChannelTopic(TOPIC_NAME);
     }
 }
