bcf6fd810581bca1453e2deb61abe6b58eb4274f
jiinse0 <jee_ji@naver.com>
2023-08-24 17:37:25 +0900

    [FEAT] redis pub/sub 구현

--- src/main/java/com/sparta/quizdemo/chat/service/RedisPublisher.java
+++ src/main/java/com/sparta/quizdemo/chat/service/RedisPublisher.java
@@ -1,6 +1,6 @@
 package com.sparta.quizdemo.chat.service;
 
-import com.sparta.quizdemo.common.entity.ChatMessage;
+import com.sparta.quizdemo.chat.entity.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
