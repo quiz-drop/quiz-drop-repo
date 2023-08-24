57cdefb6722abc488a8f094260b05a8800aca8b2
jiinse0 <jee_ji@naver.com>
2023-08-24 17:36:45 +0900

    [REFACTOR] CORS 권한 설정

--- src/main/java/com/sparta/quizdemo/QuizdemoApplication.java
+++ src/main/java/com/sparta/quizdemo/QuizdemoApplication.java
@@ -2,17 +2,28 @@ package com.sparta.quizdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class QuizdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuizdemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuizdemoApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
 }
