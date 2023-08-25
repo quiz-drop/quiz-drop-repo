a4f3a1af3bc6bbd7960e85bc3c6de33cf5bbf4d6
jiinse0 <jee_ji@naver.com>
2023-08-25 12:57:32 +0900

    [REFACTOR] corsConfigurer 메서드 삭제

--- src/main/java/com/sparta/quizdemo/QuizdemoApplication.java
+++ src/main/java/com/sparta/quizdemo/QuizdemoApplication.java
@@ -16,14 +16,4 @@ public class QuizdemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuizdemoApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
-            }
-        };
-    }
 }
