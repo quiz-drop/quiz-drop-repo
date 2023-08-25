cdea7de338819063178a8ee3c5efa9e13881d33a
yuio7279 <yuio7279@gmail.com>
2023-08-25 16:03:13 +0900

    [FEAT] : 이메일 인증기능 구현

--- src/main/java/com/sparta/quizdemo/TestController.java
+++ src/main/java/com/sparta/quizdemo/TestController.java
@@ -6,13 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping("/test")
-    String test(){
-        return "test";
-    }
-
     @GetMapping("/login-test")
     String loginTest(){
         return "login-test";
+    }
+
+
+    @GetMapping("/mail-test")
+    String mailTest(){
+        return "mail-test";
     }
 }
