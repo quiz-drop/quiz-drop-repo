e18fd6433105e61b3067adc81c860375f1d6b4b6
jiinse0 <jee_ji@naver.com>
2023-08-29 02:35:31 +0900

    :sparkles: [REFACTOR] 배달, 주문 완료 알림 구현

--- src/main/java/com/sparta/quizdemo/order/service/OrderService.java
+++ src/main/java/com/sparta/quizdemo/order/service/OrderService.java
@@ -128,6 +128,18 @@ public class OrderService {
                 }
             }
 
+            String url = "";
+            String orderUsername = order.getUser().getUsername();
+            Optional<User> orderUser = orderRepository.findByUsername(orderUsername);
+            if (orderUser.isPresent()) {
+                User receiver = orderUser.get();
+                String content = receiver + "님! 주문이 완료되었습니다!";
+                notificationService.send(receiver, NotificationType.ORDER, content, url);
+            } else {
+                log.error("User not found");
+                throw new IllegalArgumentException("존재하지 않는 유저입니다.");
+            }
+
             return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponseDto("결제가 완료 되었습니다.", HttpStatus.CREATED.value()));
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponseDto("금액을 올바르게 입력해 주세요.", HttpStatus.BAD_REQUEST.value()));
@@ -184,8 +196,8 @@ public class OrderService {
                     Optional<User> orderUser = orderRepository.findByUsername(orderUsername);
                     if (orderUser.isPresent()) {
                         User receiver = orderUser.get();
-                        String content = receiver + "님! 주문이 완료되었습니다!";
-                        notificationService.send(receiver, NotificationType.ORDER, content, url);
+                        String content = receiver + "님! 배달이 완료되었습니다!";
+                        notificationService.send(receiver, NotificationType.DELIVERY, content, url);
                     } else {
                         log.error("User not found");
                         throw new IllegalArgumentException("존재하지 않는 유저입니다.");
