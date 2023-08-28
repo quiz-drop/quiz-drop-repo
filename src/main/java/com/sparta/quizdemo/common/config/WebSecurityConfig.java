c26f8d9e19e76441485a4859cb4fd238912a9d9b
jiinse0 <jee_ji@naver.com>
2023-08-28 09:54:55 +0900

    :recycle: [REFACTOR] 인증 허용 경로 수정

--- src/main/java/com/sparta/quizdemo/common/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/quizdemo/common/config/WebSecurityConfig.java
@@ -68,9 +68,8 @@ public class WebSecurityConfig implements WebMvcConfigurer {
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .requestMatchers("/app/chat/**").permitAll()
-                        .requestMatchers("/push").permitAll()
-                        .requestMatchers("/chatRoom", "/chatting").permitAll()
-                        .requestMatchers("/login-test","/test","/api/auth/**","/api/user/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .requestMatchers("/chatRoom", "/chatting", "/notification/**").permitAll()
+                        .requestMatchers("/notification-page", "/login-test","/test","/api/auth/**","/api/user/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
 
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
