c2ee1f0e4d9c42a42068d963d0c9608fbcb0d456
wlstj <redvoca100466@gmail.com>
2023-08-30 15:44:23 +0900

    [FEAT] Redis 관련 임포트 추가

--- src/main/java/com/sparta/quizdemo/common/config/RedisConfig.java
+++ src/main/java/com/sparta/quizdemo/common/config/RedisConfig.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,6 +14,8 @@ import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.Topic;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -41,9 +45,9 @@ public class RedisConfig {
     }
 
     /*
-    * RedisMessageListenerContainer 로부터 메시지를 dispatch 받고,
-    * 실제 메시지를 처리하는 비즈니스 로직이 담긴 Subscriber Bean 추가
-    */
+     * RedisMessageListenerContainer 로부터 메시지를 dispatch 받고,
+     * 실제 메시지를 처리하는 비즈니스 로직이 담긴 Subscriber Bean 추가
+     */
     @Bean
     public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) {
         return new MessageListenerAdapter(subscriber, "onMessage");
@@ -69,5 +73,22 @@ public class RedisConfig {
     @Bean
     public Topic createTopic() {
         return new ChannelTopic(TOPIC_NAME);
+    }
+
+    //레디스 캐시
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        //key, value 형태의 값을 직렬화(REST API에서 사용하는 JSON 형태로 변환) 시켜주고 리턴
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext
+                        .SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        return RedisCacheManager
+                .RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
     }
 }
