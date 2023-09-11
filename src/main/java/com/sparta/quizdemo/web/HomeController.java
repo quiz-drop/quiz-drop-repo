4e1c0aa4f1f05ded8787a229a1ff8507ede7b426
jiinse0 <jee_ji@naver.com>
2023-09-11 20:21:24 +0900

    [refactor] view controller

--- src/main/java/com/sparta/quizdemo/web/HomeController.java
+++ src/main/java/com/sparta/quizdemo/web/HomeController.java
@@ -3,6 +3,8 @@ package com.sparta.quizdemo.web;
 import com.sparta.quizdemo.common.security.UserDetailsImpl;
 import com.sparta.quizdemo.product.entity.Product;
 import com.sparta.quizdemo.product.repository.ProductRepository;
+import com.sparta.quizdemo.user.entity.User;
+import com.sparta.quizdemo.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,14 +17,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 public class HomeController {
 
+    private final UserRepository userRepository;
     private final ProductRepository productRepository;
 
     @GetMapping("/")
     public String mainPage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        if(userDetails != null) {
+        if (userDetails != null) {
             model.addAttribute("username", userDetails.getUsername());
             model.addAttribute("userRole", userDetails.getUser().getRole());
-            model.addAttribute("userAddress", userDetails.getUser().getAddress().getAddress1());
+            if(userDetails.getUser().getAddress() != null ){
+                model.addAttribute("userAddress", userDetails.getUser().getAddress().getAddress1());
+            }else{
+                model.addAttribute("userAddress", "주소없음");
+            }
         }
         return "index";
     }
@@ -33,7 +40,7 @@ public class HomeController {
 
         model.addAttribute("category", product.getCategory());
         model.addAttribute("productId", productId);
-        if(userDetails != null) {
+        if (userDetails != null) {
             model.addAttribute("username", userDetails.getUsername());
             model.addAttribute("userRole", userDetails.getUser().getRole());
         }
@@ -49,26 +56,43 @@ public class HomeController {
 
     @Secured("ROLE_ADMIN")
     @GetMapping("/new-product")
-    public String newProduct() {
+    public String newProduct(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        model.addAttribute("username", userDetails.getUsername());
+        model.addAttribute("userRole", userDetails.getUser().getRole());
         return "newProduct";
     }
 
     @Secured("ROLE_ADMIN")
     @GetMapping("/new-option")
-    public String newOption() {
+    public String newOption(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        model.addAttribute("username", userDetails.getUsername());
+        model.addAttribute("userRole", userDetails.getUser().getRole());
         return "newOption";
     }
 
     @Secured("ROLE_ADMIN")
     @GetMapping("/optionList-page")
-    public String optionListPage() {
+    public String optionListPage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        model.addAttribute("username", userDetails.getUsername());
+        model.addAttribute("userRole", userDetails.getUser().getRole());
         return "optionList";
     }
 
     @Secured("ROLE_ADMIN")
     @GetMapping("/edit-product/{productId}")
     public String updateProduct(@PathVariable Long productId, Model model) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new NullPointerException("해당 번호의 상품이 존재하지 않습니다."));
+
         model.addAttribute("productId", productId);
+        model.addAttribute("productName", product.getProductName());
+        model.addAttribute("productPrice", product.getProductPrice());
+        model.addAttribute("productImage", product.getProductImage());
+        model.addAttribute("productIntro", product.getProductIntro());
+        model.addAttribute("cookingTime", product.getCookingTime());
+        model.addAttribute("category", product.getCategory());
         return "editProduct";
     }
 
@@ -83,32 +107,66 @@ public class HomeController {
     @Secured("ROLE_ADMIN")
     @GetMapping("/admin")
     public String backOffice(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        if(userDetails != null) {
+        if (userDetails != null) {
             model.addAttribute("username", userDetails.getUsername());
             model.addAttribute("userRole", userDetails.getUser().getRole());
         }
         return "backOffice";
     }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/user-page/{userName}")
+    public String getUserInfoPage(Model model, @PathVariable String userName) {
+        User user = userRepository.findByUsername(userName).orElseThrow(() -> new NullPointerException("해당 아이디의 유저가 존재하지 않습니다."));
+
+        model.addAttribute("userNo", user.getId());
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("nickname", user.getNickname());
+        model.addAttribute("role", user.getRole());
+        model.addAttribute("zipCode", user.getAddress().getZip_code());
+        model.addAttribute("address1", user.getAddress().getAddress1());
+        model.addAttribute("address2", user.getAddress().getAddress2());
+
+        return "user-page";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/user-info-update-page/{userName}")
+    public String updateOneUserInfoPage(Model model, @PathVariable String userName) {
+        User user = userRepository.findByUsername(userName).orElseThrow(() -> new NullPointerException("해당 아이디의 유저가 존재하지 않습니다."));
+
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("nickname", user.getNickname());
+        model.addAttribute("zipCode", user.getAddress().getZip_code());
+        model.addAttribute("address1", user.getAddress().getAddress1());
+        model.addAttribute("address2", user.getAddress().getAddress2());
+
+        return "one-user-info-update";
+    }
+
     @GetMapping("/signup")
     public String signup() {
 
         return "signup";
     }
+
     @GetMapping("/login")
-    String login(){
+    String login() {
         return "login";
     }
+
     @GetMapping("/mypage")
-    String mypage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        if(userDetails != null) {
+    String mypage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails != null) {
             model.addAttribute("username", userDetails.getUsername());
             model.addAttribute("userRole", userDetails.getUser().getRole());
         }
         return "mypage";
     }
+
     @GetMapping("/user/orders")
-    String orders(Model model,  @AuthenticationPrincipal UserDetailsImpl userDetails){
-        if(userDetails != null) {
+    String orders(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails != null) {
             model.addAttribute("username", userDetails.getUsername());
             model.addAttribute("userRole", userDetails.getUser().getRole());
         }
@@ -116,27 +174,53 @@ public class HomeController {
     }
 
     @GetMapping("/user/update")
-    String update(Model model,  @AuthenticationPrincipal UserDetailsImpl userDetails){
-        if(userDetails != null) {
+    String update(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails != null) {
             model.addAttribute("username", userDetails.getUsername());
             model.addAttribute("userRole", userDetails.getUser().getRole());
         }
         return "myUpdate";
     }
+
     @GetMapping("/user/reviews")
-    String reviews(Model model,  @AuthenticationPrincipal UserDetailsImpl userDetails){
-        if(userDetails != null) {
+    String reviews(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails != null) {
             model.addAttribute("username", userDetails.getUsername());
             model.addAttribute("userRole", userDetails.getUser().getRole());
         }
         return "myReviews";
     }
+
     @GetMapping("/user/delete")
-    String delete(Model model,  @AuthenticationPrincipal UserDetailsImpl userDetails){
-        if(userDetails != null) {
+    String delete(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails != null) {
             model.addAttribute("username", userDetails.getUsername());
             model.addAttribute("userRole", userDetails.getUser().getRole());
         }
         return "myDelete";
+    }
+
+    @GetMapping("/user/address")
+    String address(Model model,  @AuthenticationPrincipal UserDetailsImpl userDetails){
+
+        return "myAddress";
+    }
+    @GetMapping("/user/info/username")
+    String findUsername(){
+
+        return "findUsername";
+    }
+    @GetMapping("/comment")
+    String comment(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails != null) {
+            model.addAttribute("username", userDetails.getUsername());
+            model.addAttribute("userRole", userDetails.getUser().getRole());
+        }
+        return "comment";
+    }
+    @GetMapping("/user/info/password")
+    String password(){
+
+        return "findPassword";
     }
 }
