2bfb99f4296eb349f0d74fb61409a24f56408b40
jiinse0 <jee_ji@naver.com>
2023-09-11 20:19:53 +0900

    [refactor] 댓글 CRUD

---
+++ src/main/java/com/sparta/quizdemo/comment/dto/CommentRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.quizdemo.comment.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CommentRequestDto {
+
+    private String content;
+}
---
+++ src/main/java/com/sparta/quizdemo/comment/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.quizdemo.comment.repository;
+
+
+import com.sparta.quizdemo.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
