45907311e61171acf1b18c9abec093469b6b8ee3
yuio7279 <yuio7279@gmail.com>
2023-09-12 12:40:22 +0900

    [REFACTOR] : 토큰 테스트 확인

--- src/main/java/com/sparta/quizdemo/util/JwtUtil.java
+++ src/main/java/com/sparta/quizdemo/util/JwtUtil.java
@@ -31,7 +31,7 @@ public class JwtUtil {
     // Token 식별자
     public static final String BEARER_PREFIX = "Bearer ";
     // 토큰 만료시간
-    private final long TOKEN_TIME = 60 * 30 * 1000L; // 15초
+    private final long TOKEN_TIME = 60 * 30 * 1000L; // 30분
 
     @Value("${jwt.secret.key}") // Base64 Encode 한 SecretKey
     private String secretKey;
