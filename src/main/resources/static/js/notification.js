f238e09fc6972c477eb4da8c83478a86c09c3773
jiinse0 <jee_ji@naver.com>
2023-09-15 14:03:34 +0900

    [REFACTOR] 알림 JS

--- src/main/resources/static/js/notification.js
+++ src/main/resources/static/js/notification.js
@@ -73,38 +73,6 @@ $(document).ready(function () {
     }
 });
 
-// // 알림 리스트를 동적으로 생성하고 표시하는 함수
-// function displayNotifications() {
-//     // 알림 데이터를 서버에서 가져옵니다.
-//     $.ajax({
-//         url: "/api/notifications/", // 알림 데이터를 가져오는 엔드포인트 URL 설정
-//         type: "GET",
-//         contentType: "application/json",
-//         success: function (notifications) {
-//             // 알림 데이터를 받아와서 표시할 위치를 선택합니다.
-//             const notificationList = document.getElementById("notification-list");
-//
-//             // 알림 리스트를 초기화합니다.
-//             notificationList.innerHTML = "";
-//
-//             // 알림 데이터를 돌면서 리스트 아이템을 생성합니다.
-//             notifications.forEach(function (notification) {
-//                 const listItem = document.createElement("li");
-//                 listItem.textContent = notification.content;
-//                 notificationList.appendChild(listItem);
-//             });
-//         },
-//         error: function (jqXHR, textStatus, errorThrown) {
-//             // 오류 처리
-//             console.error("AJAX Error:", textStatus, errorThrown);
-//         }
-//     });
-// }
-//
-// // 페이지 로드 시 알림 리스트를 표시합니다.
-// $(document).ready(function () {
-//     displayNotifications();
-// });
 
 // 알림 전체 삭제 함수
 function deleteAllNotifications() {
