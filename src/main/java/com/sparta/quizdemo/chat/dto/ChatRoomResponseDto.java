6d1d9ee3bc15541beb210cd4bea18d4a7a8036e4
jiinse0 <jee_ji@naver.com>
2023-09-18 04:34:28 +0900

    [REFACTOR] 필드, 매개변수 추가

--- src/main/java/com/sparta/quizdemo/chat/dto/ChatRoomResponseDto.java
+++ src/main/java/com/sparta/quizdemo/chat/dto/ChatRoomResponseDto.java
@@ -1,6 +1,5 @@
 package com.sparta.quizdemo.chat.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,13 +7,14 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@Builder
 public class ChatRoomResponseDto {
     private String roomId;
+    private String userId;
     private String username;
 
-    public ChatRoomResponseDto(String roomId, String username) {
+    public ChatRoomResponseDto(String roomId, String userId, String username) {
         this.roomId = roomId;
+        this.userId = userId;
         this.username = username;
     }
 }
