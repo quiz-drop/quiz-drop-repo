1b6c8d84cfaaadb124897c0cf2aa169f818e6468
jiinse0 <jee_ji@naver.com>
2023-10-25 17:12:03 +0900

    [REFACTOR] admin 권한 유저 찾기위한 jpa 추가

--- src/main/java/com/sparta/quizdemo/user/repository/UserRepository.java
+++ src/main/java/com/sparta/quizdemo/user/repository/UserRepository.java
@@ -1,23 +1,26 @@
 
 package com.sparta.quizdemo.user.repository;
 
-import com.sparta.quizdemo.chat.entity.ChatRoom;
+import com.sparta.quizdemo.common.entity.UserRoleEnum;
 import com.sparta.quizdemo.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
     Optional<User> findByNickname(String nickname);
 
     Optional<User> findUserById(Long id);
 
     Optional<User> findBySocialIdAndSocial(String naverId, String social);
 
-
     Optional<User> findByEmail(String email);
+
+    List<User> findByRole(UserRoleEnum userRoleEnum);
 }
